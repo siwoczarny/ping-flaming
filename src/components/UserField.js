51b1222f322a644fb42131abf1e93d7fb00f9883
Maciej Chrustowski <chrustowski.maciej@gmail.com>
2020-03-18 23:06:04 +0100

    Corrections

--- src/components/UserField.js
+++ src/components/UserField.js
@@ -4,15 +4,19 @@ import userAvatar from '../images/user/oval.png';
 import Button from 'react-bootstrap/Button';
 import Dropdown from 'react-bootstrap/Dropdown';
 
+function handleClickProfile() {
+    alert('Wejście do profilu')
+}
+
 const UserField = () => {
     return (
         <div className="user">
             <img src={userAvatar} alt="avatar"></img>
             <Dropdown >
-                <Button>emil.todd_87</Button>
+                <Button onClick={handleClickProfile}>emil.todd_87</Button>
                 <Dropdown.Toggle split id="dropdown-split-basic" />
                 <Dropdown.Menu >
-                    <Dropdown.Item href="#/action-1">Wyloguj się</Dropdown.Item>
+                    <Dropdown.Item href="#">Wyloguj się</Dropdown.Item>
                 </Dropdown.Menu>
             </Dropdown>
         </div>
