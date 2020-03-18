46c83c891c12d331b5c3117c57f1ecfa8a2742ed
Maciej Chrustowski <chrustowski.maciej@gmail.com>
2020-03-18 22:29:28 +0100

    Improve UserField

--- src/components/UserField.js
+++ src/components/UserField.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import userAvatar from '../images/user/oval.png'
+import userAvatar from '../images/user/oval.png';
+//button & dropdown
+import Button from 'react-bootstrap/Button';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 const UserField = () => {
     return (
         <div className="user">
             <img src={userAvatar} alt="avatar"></img>
-            <input list="user-options" name="user-options" placeholder="emil.todd_87" disabled></input>
-            <datalist id="user-options">
-                <option value="">Log out</option>
-            </datalist>
+            <Dropdown >
+                <Button>emil.todd_87</Button>
+                <Dropdown.Toggle split id="dropdown-split-basic" />
+                <Dropdown.Menu >
+                    <Dropdown.Item href="#/action-1">Wyloguj się</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
         </div>
     );
 }
